90f6d4060de28f8e76236265307c9a7c7ed32829
agneshew <agnes.hewusz@gmail.com>
2020-09-27 22:35:36 +0200

    ex. 15.3

--- src/main/java/com/crud/tasks/service/DbService.java
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -3,10 +3,12 @@ package com.crud.tasks.service;
 import com.crud.tasks.domain.Task;
 import com.crud.tasks.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class DbService {
     @Autowired
     private TaskRepository repository;
@@ -15,7 +17,18 @@ public class DbService {
         return repository.findAll();
     }
 
-    public Task getTaskId(Long taskId) {
-        return repository.findById(taskId).get();
+//    public Task getTaskId(final Long id) {
+//        return repository.findById(id).orElse(null);
+//    }
+
+    public Task saveTask(final Task task) {
+        return repository.save(task);
+    }
+
+    public Optional<Task> getTask(final Long id) {
+        return repository.findById(id);
+    }
+    public void deleteTask(final Long id) {
+        repository.deleteById(id);
     }
 }
