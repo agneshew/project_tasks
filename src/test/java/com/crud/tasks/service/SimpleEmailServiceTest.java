3e56a9964ad665f4a1bac20782060e74479029fa
agneshew <agnes.hewusz@gmail.com>
2020-10-21 18:59:15 +0200

    ex. 19.3

--- src/test/java/com/crud/tasks/service/SimpleEmailServiceTest.java
+++ src/test/java/com/crud/tasks/service/SimpleEmailServiceTest.java
@@ -30,6 +30,7 @@ public class SimpleEmailServiceTest {
         mailMessage.setTo(mail.getMailTo());
         mailMessage.setSubject(mail.getSubject());
         mailMessage.setText(mail.getMessage());
+
         if  (!mail.getToCc().isEmpty()) {
             mailMessage.setCc(mail.getToCc());
         }
